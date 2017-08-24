c1195d3dfd5088298a2cf5662ac9059728ca80c1
sangaviselva <31208515+sangaviselva@users.noreply.github.com>
2017-08-24 13:36:18 +0530

    Add files via upload

---
+++ exce7.py
@@ -0,0 +1,19 @@
+print  "Mary had a little lamb."
+print  "Its fleece was white as %s."% 'snow'
+print  "And everywhere that Mary went."
+print  "." * 10 # what 'd that do?
+eg1="C"
+eg2="h"
+eg3="e"
+eg4="e"
+eg5="s"
+eg6="e"
+eg7="b"
+eg8="u"
+eg9="r"
+eg10="g"
+eg11="e"
+eg12="r"
+print  eg1+eg2+eg3+eg4+eg5+eg6,
+print  eg7+eg8+eg9+eg10+eg11+eg12
+ 
